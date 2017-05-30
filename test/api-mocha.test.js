9be1276bb49abe3c9d1c9bc44f217e178b48f71c
Douglas Rafael <douglasrafaelcg@gmail.com>
2017-05-30 03:39:44 -0300

    Refatoração, adaptando para arrow functions

--- test/api-mocha.test.js
+++ test/api-mocha.test.js
@@ -9,19 +9,19 @@ const request = require('supertest').agent(app);
 
 const Task = require('../models/task');
 
-describe('Teste de API - Rotas', function () {
+describe('Teste de API - Rotas', () => {
 
     var id;
     var task;
     var task_post;
 
     // É executado ANTES de todos os testes neste bloco
-    before(function (done) {
+    before((done) => {
         task = new Task({
             title: 'Task inserida para teste de api',
             completionDate: '2017-05-29',
             labels: ['teste', 'api', 'rotas', 'mocha', 'chai']
-        }).save(function (err, res) { // Insere
+        }).save((err, res) => { // Insere
             if (err) return done(err);
 
             task = res;
@@ -30,56 +30,56 @@ describe('Teste de API - Rotas', function () {
     });
 
     // É executado APÓS todos os testes neste bloco
-    after(function () {
+    after(() => {
         removeTask(task._id);
     });
 
-    describe('ROTAS GET', function () {
-        it('GET /api/task - Deve retornar status 404 quando rota for inválida.', function (done) {
+    describe('ROTAS GET', () => {
+        it('GET /api/task - Deve retornar status 404 quando rota for inválida.', (done) => {
             request
                 .get('/api/task')
                 .expect(404)
-                .end(function (err, res) {
+                .end((err, res) => {
                     expect(res.statusCode).to.equal(404);
                     done();
                 });
         });
 
-        it('GET /api/tasks - Deve retornar lista de tasks.', function (done) {
+        it('GET /api/tasks - Deve retornar lista de tasks.', (done) => {
             request
                 .get('/api/tasks')
                 .expect("Content-type", /json/)
                 .expect(200)
-                .end(function (err, res) {
+                .end((err, res) => {
                     expect(res.statusCode).to.equal(200);
                     expect(res.charset).to.equal('utf-8');
 
                     expect(res.error).to.be.false;
                     expect(res.body).to.be.an('array');
 
-                    // Espera pelo menos um registro
+                    // Espera pelo menos um registro >=
                     expect(res.body.length).to.be.at.least(1);
 
                     done();
                 });
         });
 
-        it('GET /api/tasks/12 - Retorna status 404 quando task não for encontrada.', function (done) {
+        it('GET /api/tasks/12 - Retorna status 404 quando task não for encontrada.', (done) => {
             request
                 .get('/api/tasks/12')
                 .expect(404)
-                .end(function (err, res) {
+                .end((err, res) => {
                     expect(res.statusCode).to.equal(404);
                     done();
                 });
         });
 
-        it('GET /api/tasks/:_id - Deve retornar uma task de acordo com o _id.', function (done) {
+        it('GET /api/tasks/:_id - Deve retornar uma task de acordo com o _id.', (done) => {
             request
                 .get('/api/tasks/' + task._id)
                 .expect("Content-type", /json/)
                 .expect(200)
-                .end(function (err, res) {
+                .end((err, res) => {
                     expect(res.statusCode).to.equal(200);
                     expect(res.body).to.be.an('object');
 
@@ -95,15 +95,15 @@ describe('Teste de API - Rotas', function () {
         });
     });
 
-    describe('ROTAS POST', function () {
-        it('POST /api/tasks	- Não deve inserir uma task sem title e completionDate', function (done) {
+    describe('ROTAS POST', () => {
+        it('POST /api/tasks	- Não deve inserir uma task sem title e completionDate', (done) => {
             request
                 .post('/api/tasks')
                 .send({
                     description: 'Tentando inserir sem os campos obrigatórios: title e completionDate'
                 })
                 .expect(400)
-                .end(function (err, res) {
+                .end((err, res) => {
                     expect(res.statusCode).to.equal(400);
                     expect(res.body).to.be.an('object');
                     expect(res.body).to.have.property('errors');
@@ -111,7 +111,7 @@ describe('Teste de API - Rotas', function () {
                 })
         });
 
-        it('POST /api/tasks	- Deve inserir uma nova task e retornar o objeto inserido.', function (done) {
+        it('POST /api/tasks	- Deve inserir uma nova task e retornar o objeto inserido.', (done) => {
             let t = {
                 title: 'Task inserida usando rota POST /api/tasks',
                 description: 'Descrição da task',
@@ -127,7 +127,7 @@ describe('Teste de API - Rotas', function () {
                 .send(t)
                 .expect("Content-type", /json/)
                 .expect(201)
-                .end(function (err, res) {
+                .end((err, res) => {
                     expect(res.statusCode).to.equal(201);
                     expect(res.body).to.be.an('object');
 
@@ -151,19 +151,19 @@ describe('Teste de API - Rotas', function () {
         });
     });
 
-    describe('ROTAS PUT', function () {
-        it('PUT /api/tasks/:_id - Deve retornar erro 404 quando a task a ser atualizada não existir.', function (done) {
+    describe('ROTAS PUT', () => {
+        it('PUT /api/tasks/:_id - Deve retornar erro 404 quando a task a ser atualizada não existir.', (done) => {
             request
                 .put('/api/tasks/12')
                 .send({ title: 'Não atualizar!!!' })
                 .expect(404)
-                .end(function (err, res) {
+                .end((err, res) => {
                     expect(res.statusCode).to.equal(404);
                     done();
                 });
         });
 
-        it('PUT /api/tasks/:_id - Deve atualizar uma task de acordo com o _id e retornar o objeto atualizado.', function (done) {
+        it('PUT /api/tasks/:_id - Deve atualizar uma task de acordo com o _id e retornar o objeto atualizado.', (done) => {
             let t = {
                 title: 'Task foi atualizada',
                 isFinalized: true
@@ -174,7 +174,7 @@ describe('Teste de API - Rotas', function () {
                 .send(t)
                 .expect("Content-type", /json/)
                 .expect(201)
-                .end(function (err, res) {
+                .end((err, res) => {
                     expect(res.statusCode).to.equal(201);
                     expect(res.body).to.be.an('object');
                     expect(res.body.title).to.equal(t.title);
@@ -185,25 +185,25 @@ describe('Teste de API - Rotas', function () {
         });
     });
 
-    describe('ROTAS DELETE', function () {
-        it('PUT /api/tasks/:_id - Deve retornar erro 404 quando a task a ser removida não existir.', function (done) {
+    describe('ROTAS DELETE', () => {
+        it('PUT /api/tasks/:_id - Deve retornar erro 404 quando a task a ser removida não existir.', (done) => {
             request
                 .delete('/api/tasks/12')
                 .expect(404)
-                .end(function (err, res) {
+                .end((err, res) => {
                     expect(res.statusCode).to.equal(404);
                     done();
                 });
         });
 
-        it('DELETE /api/tasks/:_id - Deve remover uma task de cordo com o _id e retornar o objeto removido.', function (done) {
+        it('DELETE /api/tasks/:_id - Deve remover uma task de cordo com o _id e retornar o objeto removido.', (done) => {
             // Insere novamente uma nova task para depois tentar remover pela rota
             task.save(function (err, t) {
                 request
                     .delete('/api/tasks/' + t._id)
                     .expect("Content-type", /json/)
                     .expect(200)
-                    .end(function (err, res) {
+                    .end((err, res) => {
                         // expect(res.body).to.be.an('object');
                         done();
                     });
@@ -213,7 +213,7 @@ describe('Teste de API - Rotas', function () {
 });
 
 function removeTask(id) {
-    Task.findByIdAndRemove(id, function (err, res) {
+    Task.findByIdAndRemove(id, (err, res) => {
         if (err) return done(err);
         done();
     });
